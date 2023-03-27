11666106df8a953c3739b4e8664a6b78dbeffa66
gustavo ramon dos reis <gustavobilireis@hotmail.com>
2023-03-27 14:53:48 -0300

    corrige erros

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -16,11 +16,10 @@ def read(path: str) -> List[Dict]:
 
 def get_unique_job_types(path: str) -> List[str]:
     data = read(path)
-    conjunto = set()
+    result = set()
     for index in data:
-        result = index["job_type"]
-        conjunto.add(result)
-    return conjunto
+        result.add(index["job_type"])
+    return result
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
@@ -35,5 +34,5 @@ def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
 if __name__ == "__main__":
     data_read = read("data/jobs.csv")
     data_get_unique_job_types = get_unique_job_types("data/jobs.csv")
-    print(filter_by_job_type(data_read, 'OTHER'))
+    print(filter_by_job_type(data_read, "OTHER"))
     print(data_get_unique_job_types)
